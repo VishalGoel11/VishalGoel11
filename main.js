e4c3c6ee631cefed0e87be326c1c4b6896ba4754
VishalGoel11 <vishugoel2812@gmail.com>
2023-10-01 21:46:50 +0530

    landing page updated

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const ex = document.getElementById('ex');
 const ab = document.getElementById('ab');
  const pr = document.getElementById('pr');
+ const icon = document.getElementById('line3');
+ const cross = document.getElementById('cross');
+ cross.style.display="none";
+ let a=1;
+ cross.addEventListener("click",()=>{
+    if(a===0){
+        icon.style.display="flex"
+        cross.style.display="none"
+        a=1;
+    }
+ })
+ icon.addEventListener("click",()=>{
+    if(a===1){
+        icon.style.display="none"
+        cross.style.display="flex"
+        a=0;
+    }
+ })
 //  class="flex flex-wrap md:justify-between"  id="ab" style="text-align: center;"
 if(innerWidth<='700'){
 
