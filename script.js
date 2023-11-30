ab8da8a552ea63a8ca2b96a774834a5aa7b21f61
Vinay Sharma <vinaysharma511511@gmail.com>
2023-11-30 20:26:32 +0530

    initialcommit

---
+++ script.js
@@ -0,0 +1,51 @@
+const menuBtn = document.querySelector(".menuBtn");
+
+menuBtn.addEventListener("click", () => {
+  document.querySelector(".mobile").classList.toggle("hidden");
+});
+
+let words = ["Hi I'm Vinay Sharma", "A Web Developer"],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+
+let wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
